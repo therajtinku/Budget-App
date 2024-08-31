690e9e8d2727cbc48621221b06b5e2fb4dc5ee74
therajtinku <rajutinku27@gmail.com>
2024-08-31 20:17:04 +0530

    added budget form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import { fetchData } from "../helpers"
 
 //components
 import Intro from "../components/Intro"
+import AddBudgetForm from "../components/AddBudgetForm"
 
 //library toast important
 import { toast } from "react-toastify"
@@ -13,7 +14,8 @@ import { toast } from "react-toastify"
 //Loader
 export function dashboardLoader(){
     const userName = fetchData("userName")
-    return {userName}
+    const budgets = fetchData("budgets")
+    return {userName, budgets}
 
 }
 
@@ -32,11 +34,23 @@ export async function dashboardAction({request}) {
 
 export const Dashboard = () => {
 
-    const { userName } = useLoaderData();
+    const { userName, budgets } = useLoaderData();
 
   return (
     <>
-        {userName ? (<p>{userName}</p>) : <Intro /> }
+        {userName ? (
+          <div className="dashboard">
+            <h1>Welcome back, <span className="accent">{userName}</span></h1>
+            <div className="grid-sm">
+              {/* {budgets ? () : ()} */}
+              <div className="grid-lg">
+                <div className="flex-lg">
+                  <AddBudgetForm />
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : <Intro /> }
     </>
   )
 }
