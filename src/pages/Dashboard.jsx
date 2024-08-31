ccf7519fbac30107379ce5463a6d5bb3bba1a43c
therajtinku <rajutinku27@gmail.com>
2024-08-31 20:56:22 +0530

    added colors and handled multiple forms

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@
 import { useLoaderData } from "react-router-dom"
 
 //helpers functions
-import { fetchData } from "../helpers"
+import { createBudget, fetchData } from "../helpers"
 
 //components
 import Intro from "../components/Intro"
@@ -22,13 +22,28 @@ export function dashboardLoader(){
 //action
 export async function dashboardAction({request}) {
   const data = await request.formData();
-  const formData = Object.fromEntries(data);
-  try {
-    localStorage.setItem("userName", JSON.stringify(formData.userName))
-    return toast.success(`Welcome, ${formData.userName}`)
-  } catch (e) {
-    throw new Error("There was a problem creating your account");
-    
+  const {_action, ...values } = Object.fromEntries(data);
+
+  if(_action === "newUser"){
+    try {
+      localStorage.setItem("userName", JSON.stringify(values.userName))
+      return toast.success(`Welcome, ${values.userName}`)
+    } catch (e) {
+      throw new Error("There was a problem creating your account");
+    }
+  }
+
+  if(_action === "createBudget"){
+    try{
+      //create budget
+      createBudget({
+        name: values.newBudget,
+        amount: values.newBudgetAmount,
+      })
+      return toast.success("Budget Created!")
+    }catch(e){
+      throw new Error("There was a problem creating your Budget")
+    }
   }
 }
 
